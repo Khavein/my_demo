600b8c21b734e010726b3288af3f71ccd2b69b59
Khavein <khavein@hotmail.my>
2024-03-04 14:47:48 +0800

    Initial Commit
    This is just a Plain Flutter Project

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page by Khavein'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
